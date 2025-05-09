ad691ab615fcd3be67e3a76fb34f7195345112df
Acv3-1 <149637706+Acv3-1@users.noreply.github.com>
2025-05-09 09:21:54 -0500

    base de datos

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Configuración de la base de datos SQLite
+# Configuración de la base de datos SQLite cambios en la bd
 DATABASE_URL = "sqlite:///biblioteca.db"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
